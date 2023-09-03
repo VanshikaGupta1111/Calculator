2105e714f4f0976e8fdc675c5d60cd4fe5c7763a
Vanshika Gupta <142769234+VanshikaGupta14@users.noreply.github.com>
2023-09-03 10:14:38 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+let string="";
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button)=>{
+        button.addEventListener('click',(e)=>{
+           if (e.target.innerHTML == '='){
+               string = eval(string);
+               document.querySelector('input').value = string;
+            }
+            else if (e.target.innerHTML == 'AC'){
+                string = "";
+                document.querySelector('input').value = string;
+            }
+            else if(e.target.innerHTML == 'fa-solid fa-delete')
+            {
+                strings = strings.substring(0,strings.length-1);
+                document.querySelector('input').value = strings;
+            }
+            else{
+                console.log(e.target)
+                string = string + e.target.innerHTML;
+                document.querySelector('input').value = string;
+            }
+        })
+    })
