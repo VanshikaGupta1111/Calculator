5ca15a19ea87c5b70dc15b8601f6659c1faeee68
Vanshika Gupta <142769234+VanshikaGupta14@users.noreply.github.com>
2023-09-05 09:25:16 +0530

    script.js

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
-let string="";
-let buttons = document.querySelectorAll('.button');
-Array.from(buttons).forEach((button)=>{
-        button.addEventListener('click',(e)=>{
-           if (e.target.innerHTML == '='){
-               string = eval(string);
-               document.querySelector('input').value = string;
-            }
-            else if (e.target.innerHTML == 'AC'){
-                string = "";
-                document.querySelector('input').value = string;
-            }
-            else if(e.target.innerHTML == 'DEL')
-            {
-                strings = strings.substring(0,strings.length-1);
-                document.querySelector('input').value = strings;
-            }
-            else{
-                console.log(e.target)
-                string = string + e.target.innerHTML;
-                document.querySelector('input').value = string;
-            }
-        })
+let input = document.getElementById('inputBox');
+let strings = " ";
+let buttons = document.querySelectorAll('button');
+let arr = Array.from(buttons);
+arr.forEach((button) => {
+    button.addEventListener('click', (e)=>{
+        if(e.target.innerHTML == '='){
+            strings = eval(strings);
+            document.querySelector('input').value = strings;
+        }
+        else if(e.target.innerHTML == 'DEL')
+        {
+            strings = strings.substring(0,strings.length-1);
+            document.querySelector('input').value = strings;
+        }
+        else if (e.target.innerHTML == 'AC'){
+            strings = "";
+            document.querySelector('input').value = strings;
+        }
+        else{
+            console.log(e.target)
+            strings += e.target.innerHTML;
+            // input.value = strings;
+            document.querySelector('input').value = strings;
+        }
+        
     })
+})
